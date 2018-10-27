ad7130145283e06871a9e84f44f0f3c573cc8b4e
IamFoka <iamfoka@gmail.com>
2018-10-27 16:55:34 -0300

    Set cursor to option at menu

--- minesweeper.asm
+++ minesweeper.asm
@@ -30,10 +30,10 @@ SET_DATA_SEG proc
 endp
 
 SET_SCREEN_SEG proc  
-    push ax
+    push ax 
  
-    mov ax, screen_start
-	mov es, ax
+    mov ax, screen_start ; uses ax as temporary variable
+	mov es, ax ; moves screen start
 	xor di, di 
 	
 	pop ax
@@ -42,25 +42,88 @@ endp
 
 WRITE_SCREEN_STRING proc
 	LOOP_WRITE_SCREEN_STRING:
-	movsb
-	mov es:di, bl
-	inc di
+	movsb ; movsb '==' mov es:di, ds:si
+	mov es:di, bl ; add character attributes (color)
+	inc di ; set cursor to the next character
 	loop LOOP_WRITE_SCREEN_STRING 
 	ret
 endp
 
+SHOW_MENU proc
+	;movsb '==' mov es:di, ds:si
+	;--------------------title------------------------
+	mov si, offset minesweeper ; set string offset
+	mov di, 0B2H ; set screen buffer offset
+	cld ; indicates cursor direction
+	mov bl, 4H ; set color (red = 4H)
+	mov cx, 22 ; set string lenght
+
+	call WRITE_SCREEN_STRING ; prints string on the screen buffer
+
+	add di, 68H ; moves cursor to the next string position on screen (already centralized)(1 line = 50H)
+
+	;--------------------AUTHORS------------------------
+	mov si, offset authors ; set string offset
+	mov cx, 34 ; set string lenght
+	mov bl, 7H  ; set color (light gray = 7H)
+
+	call WRITE_SCREEN_STRING ; prints string on the screen buffer
+
+	add di, 0B6H ; moves cursor to the next string position on screen (already centralized)(1 line = 50H)
+
+	;--------------------configuration------------------------
+
+	mov si, offset configuration ; set string offset
+	mov cx, 24 ; set string lenght
+	mov bl, 2H  ; set color (green = 2H)
+
+	call WRITE_SCREEN_STRING; prints string on the screen buffer
+
+	add di, 0CEH ; moves cursor to the next string position on screen (already indentated)(1 line = 50H)
+
+	;----------------------------------------------------
+	;--------------------configuration------------------------
+	mov si, offset minesamount ; set string offset
+	mov cx, 22 ; set string lenght
+	mov bl, 0FH   ; set color (white = 0FH)
+
+	call WRITE_SCREEN_STRING ; prints string on the screen buffer
+	add di, 74H ; moves cursor to the next string position on screen (already indentated)(1 line = 50H)
 
 
+	mov si, offset boardwidht ; set string offset
+	mov cx, 24 ; set string lenght
 
+	call WRITE_SCREEN_STRING ; prints string on the screen buffer
+	add di, 70H ; moves cursor to the next string position on screen (already indentated)(1 line = 50H)
 
+	mov si, offset boardheight ; set string offset
+	mov cx, 23 ; set string lenght
 
-; color all characters: 
-;mov cx, 12  ; number of characters. 
-;mov di, 03h ; start from byte after 'h' 
+	call WRITE_SCREEN_STRING ; prints string on the screen buffer
+	ret
+endp
 
-;c:  mov [di], 11001110b   ; light red(1100) on yellow(1110) 
-;    add di, 2 ; skip over next ascii code in vga memory. 
-;    loop c
+INSERT_GAME_CONFIGURATION proc
+    mov ah, 2H ; function 2 on 10H interruption = set cursor position
+    xor bh, bh ; page number (0 for graphics mode) from interruption 10H
+	mov dh, 10
+	mov dl, 37	
+	int 10H
+	
+	mov ah, 0
+	int 16h
+	
+	ret
+endp
+
+WRITE_CHAR proc ; write the character on DL reg
+    push AX
+    mov AH, 2 ; function 2 on 21H interruption: write character to standard output.
+    int 21H
+    pop AX
+    ret
+endp
 
 ; wait for any key press: 
 ;mov ah, 0
@@ -72,62 +135,7 @@ call SCREEN_MODE
 call SET_DATA_SEG
 call SET_SCREEN_SEG
 
-;movsb copia de mov es:di, ds:si
-;--------------------title------------------------
-mov si, offset minesweeper ;
-mov di, 0B2H
-cld
-mov bl, 04H
-mov cx, 22
-
-call WRITE_SCREEN_STRING
-
-add di, 68H
-;-----------------------------------------------------
-;--------------------AUTHORS------------------------
-mov si, offset authors
-mov cx, 34
-mov bl, 7H
-
-call WRITE_SCREEN_STRING
-
-add di, 0B6H
-;----------------------------------------------------
-;--------------------configuration------------------------
-
-mov si, offset configuration
-mov cx, 24
-mov bl, 2H
-
-call WRITE_SCREEN_STRING
-
-add di, 016EH
-
-;----------------------------------------------------
-;--------------------configuration------------------------
-mov si, offset minesamount
-mov cx, 22
-mov bl, 0FH
-
-call WRITE_SCREEN_STRING
-add di, 74H
-
-
-mov si, offset boardwidht
-mov cx, 24
-
-call WRITE_SCREEN_STRING
-add di, 70H
-
-mov si, offset boardheight
-mov cx, 23
-
-call WRITE_SCREEN_STRING
-
-
-
-
-
-
+call SHOW_MENU
+call INSERT_GAME_CONFIGURATION
 
 end start
